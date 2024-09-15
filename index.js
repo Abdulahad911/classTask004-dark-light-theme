12612ebd7fc9b2886995d79cb30013be579a1387
Abdulahad911 <a.a.khan6204@gmail.com>
2024-09-14 22:27:42 -0700

    completed

--- index.js
+++ index.js
@@ -2,15 +2,18 @@
 document.getElementById('colorToggleBtn').addEventListener('click', function() {
     
     var currentColor = document.body.style.backgroundColor;
-   
+    
     
     if (currentColor === 'black') {
         document.body.style.backgroundColor = 'white';
-        document.getElementById('colorToggleBtn').innerText ='Click here to change the change it into black'
-       
+        document.getElementById('colorToggleBtn').innerText ='NIGHT'
+         document.getElementById('colorToggleBtn').style.backgroundColor = 'black'
+        document.getElementById('colorToggleBtn').style.color = 'white'
     }
      else {
         document.body.style.backgroundColor = 'black';
-       document.getElementById('colorToggleBtn').innerText ='Click here to change the change it into white'
+       document.getElementById('colorToggleBtn').innerText ='DAY'
+       document.getElementById('colorToggleBtn').style.backgroundColor = 'white'
+      document.getElementById('colorToggleBtn').style.color = 'black'
     }
 });
